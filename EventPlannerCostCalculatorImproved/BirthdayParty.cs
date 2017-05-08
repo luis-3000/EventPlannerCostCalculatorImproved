81987736a2ea8f569469b3dcaae7cc3187fc3d88
Jose Castillo <Jose Castillo>
2017-05-07 22:30:51 -0500

    Uploading initial files

---
+++ EventPlannerCostCalculatorImproved/BirthdayParty.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventPlannerCostCalculatorImproved
+{
+    class BirthdayParty
+    {
+        public const int CostOfFoodPerPerson = 25;
+
+        public int NumberOfPeople { get; set; }
+
+        public bool FancyDecorations { get; set; }
+
+        public string CakeWriting { get; set; }
+
+        /* The constructor sets up the object's state by setting the properties so that it can calculate
+         * the cost later.*/
+        public BirthdayParty(int numberOfPeople, bool fanceDecorations, string cakeWriting)
+        {
+            this.NumberOfPeople = numberOfPeople;
+            this.FancyDecorations = fanceDecorations;
+            this.CakeWriting = cakeWriting;
+        }
+
+        /* A private property that calculates the actual length of the writing to use for the calculation. 
+         * If the writing is too long for the cake, this property calculates the actual number of letters
+         * that will fit on the cake. */
+        private int ActualLength
+        {
+            get
+            {
+                if (CakeWriting.Length > MaxWritingLength())
+                    return MaxWritingLength();
+                else
+                    return CakeWriting.Length;
+            }
+        }
+
+        private int CakeSize()
+        {
+            if (NumberOfPeople <= 4)
+                return 8;
+            else
+                return 16;
+        }
+
+        private int MaxWritingLength()
+        {
+            if (CakeSize() == 8)
+                return 16;
+            else
+                return 40;
+        }
+
+        /* This property treutns true if the writinng is too long for the cake.
+         * we'll use it to display a "TOO LONG" message to the user. */
+        public bool CakeWritingTooLong
+        {
+            get
+            {
+                if (CakeWriting.Length > MaxWritingLength())
+                    return true;
+                else
+                    return false;
+            }
+        }
+
+        /* Just like the one in the DinnerParty class. */
+        private decimal CalculateCostOfDecorations()
+        {
+            decimal costOfDecorations;
+            if (FancyDecorations)
+                costOfDecorations = (NumberOfPeople * 15.00M) + 50M;
+            else
+                costOfDecorations = (NumberOfPeople * 7.50M) + 30M;
+            return costOfDecorations;
+        }
+
+        /* The BirthdayParty class has a decimal Cost property, just like DinnerParty. But it does
+         * a different calculation that uses the CakeSize() method and actualLength field (which is set by the
+         * CakeWriting property). */
+        public decimal Cost
+        {
+            get
+            {
+                decimal totalCost = CalculateCostOfDecorations();
+                totalCost += CostOfFoodPerPerson * NumberOfPeople;
+                decimal cakeCost;
+                if (CakeSize() == 8)
+                    cakeCost = 40M + ActualLength * .25M;
+                else
+                    cakeCost = 75M + ActualLength * .25M;
+                return totalCost + cakeCost;
+            }
+        }
+    }
+}
---
+++ EventPlannerCostCalculatorImproved/DinnerParty.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventPlannerCostCalculatorImproved
+{
+    class DinnerParty
+    {
+        public const int CostOfFoodPerPerson = 25;
+
+        // These properties are set in the constructor and updated by the form,
+        // and they're used when calculating the cost.
+        public int NumberOfPeople { get; set; }
+
+        public bool FancyDecorations { get; set; }
+
+        public bool HealthyOption { get; set; }
+
+        /* The constructor sets the three properties based on the valued passed into it by the form. */
+        public DinnerParty(int numberOfPeople, bool healthyOption, bool fancyDecorations)
+        {
+            this.NumberOfPeople = numberOfPeople;
+            this.HealthyOption = healthyOption;
+            this.FancyDecorations = fancyDecorations;
+        }
+
+        /* By making this method private, I make sure that it can't be accessed from outside
+         * of the class, which will keep it from being misused. */
+        private decimal CalculateCostOfDecorations()
+        {
+
+            decimal costOfDecorations = 0;
+
+            if (FancyDecorations)
+            {
+                costOfDecorations = (15.00M * NumberOfPeople) + 50.0M;
+            }
+            else
+            {
+                costOfDecorations = (7.50M * NumberOfPeople) + 30.0M;
+            }
+
+            return costOfDecorations;
+        }
+
+        /* The private methods used in the cost calculation such as this method, access the
+         * properties so that they have the latest information from the form. */
+        private decimal CalculateCostOfBeveragesPerPerson()
+        {
+
+            decimal costOfBeveragesPerPerson;
+
+            if (HealthyOption)
+            {
+                costOfBeveragesPerPerson = 5.00M;
+            }
+            else
+            {
+                costOfBeveragesPerPerson = 20.00M;
+            }
+
+            return costOfBeveragesPerPerson;
+        }
+
+        /* Now that the calculations are private and encapsulated behind the Cost property, there's no way for 
+         * the form to recalculate the cost of the decorations that doen't use the currently selected options.
+         * This has fixed the bug that changed the costs incorrectly every time the options were changed. */
+        public decimal Cost
+        {
+
+            get
+            {
+                decimal totalCost = CalculateCostOfDecorations();
+                totalCost += ((CalculateCostOfBeveragesPerPerson() + CostOfFoodPerPerson) * NumberOfPeople);
+
+                if (HealthyOption)
+                {
+                    // Apply the discount
+                    totalCost *= 0.95M; // Apply the 5% discount to the overall event cost if the non-alcoholic option was chosen
+                }
+
+                return totalCost;
+            }
+
+        }
+
+    }
+}
